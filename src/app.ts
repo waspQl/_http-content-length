fa14fb1d96366e4c62b56ee215ae9982b1709498
sakutaro <u390862@gmail.com>
2020-02-23 15:19:52 +0900

    add: ファイルを追加

---
+++ src/app.ts
@@ -0,0 +1,79 @@
+import Express = require('express')
+import path = require('path') // 標準モジュール
+import fetch from 'node-fetch'
+
+import assert from 'assert'
+
+// 引数が渡ってきた場合はそれをport番号として使う
+let port = 3000
+if (
+  process.argv[2]
+  && Number.isInteger(
+    parseInt(process.argv[2])
+  )
+) port = parseInt(process.argv[2])
+
+const app = Express()
+
+// localのimagesディレクトリにfilesパスでアクセス出来るように
+app.use('/files', Express.static(
+  path.join(__dirname, '/../images')
+))
+// localのjsonsディレクトリにfilesパスでアクセス出来るように
+app.use('/files', Express.static(
+  path.join(__dirname, '/../jsons')
+))
+
+app.get('/', (req, res) => {
+  res.send('GET is sended.')
+});
+
+app.post('/', (req, res) => {
+  res.send('POST is sended.')
+});
+
+app.listen(port, () => {
+  console.log('server is up')
+
+  const url = `http://localhost:${port}/`
+
+  // 第二引数のmethod: 'GET'はdefault値のため省略可
+  let imageLength: number
+  fetch(url + 'files/pokemon.png', { method: "GET" })
+    .then(res => {
+      if (res.ok) {
+        // Content-Typeはimage/png
+        assert.equal(res.headers.get('Content-Type'), 'image/png')
+        imageLength = parseInt(res.headers.get('Content-Length') || '0', 10)
+
+        return res.buffer() // res.text() だとサイズが変わる
+      }
+      throw new Error('Network response was not ok.')
+    })
+    .then(buffer => {
+      assert.equal(imageLength, buffer.length)
+    })
+    .catch((err) => {
+      console.log('There has been a problem with fetch image operation: ', err)
+    })
+
+  // 第二引数のmethod: 'GET'はdefault値のため省略可
+  let jsonLength: number
+  fetch(url + 'files/tenki.json', { method: "GET" })
+    .then(res => {
+      if (res.ok) {
+        // Content-Typeはapplication/json
+        assert.equal(res.headers.get('Content-Type'), 'application/json; charset=UTF-8')
+        jsonLength = parseInt(res.headers.get('Content-Length') || '0', 10)
+
+        return res.text()
+      }
+      throw new Error('Network response was not ok.')
+    })
+    .then(text => {
+      assert.equal(jsonLength, text.length)
+    })
+    .catch((err) => {
+      console.log('There has been a problem with fetch json operation: ', err)
+    })
+});
